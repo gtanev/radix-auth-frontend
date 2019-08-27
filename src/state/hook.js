8424f22c7be81a6192eed305b0e9ec188621dc3a
George Tanev <georgetjazz@gmail.com>
2019-08-27 22:37:14 +0200

    Persist global state to local storage with the `useEffect` and `useReducer` hooks.

--- src/state/hook.js
+++ src/state/hook.js
@@ -1,11 +1,24 @@
-import React, { createContext, useReducer, useContext } from "react";
+import React, { createContext, useContext, useEffect, useReducer } from "react";
 
 export const StateContext = createContext(null);
 
 export const ContextProvider = ({ children, reducer, initialState }) => (
-  <StateContext.Provider value={useReducer(reducer, initialState)}>
+  <StateContext.Provider value={useLocallyPersistedReducer(reducer, initialState)}>
     {children}
   </StateContext.Provider>
 );
 
 export const useGlobalState = () => useContext(StateContext);
+
+const useLocallyPersistedReducer = (reducer, defaultState, storageKey) => {
+  const hookVars = useReducer(reducer, defaultState, (defaultState) => {
+    const persisted = JSON.parse(localStorage.getItem(storageKey));
+    return persisted !== null ? persisted : defaultState
+  });
+
+  useEffect(() => {
+    localStorage.setItem(storageKey, JSON.stringify(hookVars[0]))
+  }, [storageKey, hookVars[0]]);
+
+  return hookVars;
+};
