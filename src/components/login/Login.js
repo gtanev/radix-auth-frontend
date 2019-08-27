373324f8592b0265e4081bbdac85085787f548ed
George Tanev <georgetjazz@gmail.com>
2019-08-27 22:23:57 +0200

    Simplify global state hook propagation to class components.

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -2,18 +2,16 @@ import React from "react";
 import LoginForm from "./LoginForm";
 import FormHeader from "../common/FormHeader";
 import FormFooter from "../common/FormFooter";
-import { StateContext } from "../../state/hook";
+import { useGlobalState } from "../../state/hook";
 
 export default function Login(props) {
+  const [state, dispatch] = useGlobalState();
+
   return (
-    <StateContext.Consumer>
-      {([state, dispatch]) => (
-        <div className="form-container">
-          <FormHeader/>
-          <LoginForm location={props.history.location} dispatch={dispatch}/>
-          <FormFooter text="Don't have an account?" linkLabel="Sign up" linkTarget="/register"/>
-        </div>
-      )}
-    </StateContext.Consumer>
+    <div className="form-container">
+      <FormHeader/>
+      <LoginForm location={props.history.location} globalState={state} dispatch={dispatch}/>
+      <FormFooter text="Don't have an account?" linkLabel="Sign up" linkTarget="/register"/>
+    </div>
   );
 }
--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -2,18 +2,16 @@ import React from "react";
 import SignupForm from "./SignupForm";
 import FormHeader from "../common/FormHeader";
 import FormFooter from "../common/FormFooter";
-import { StateContext } from "../../state/hook";
+import { useGlobalState } from "../../state/hook";
 
 export default function Signup() {
+  const [state, dispatch] = useGlobalState();
+
   return (
-    <StateContext.Consumer>
-      {([state, dispatch]) => (
-        <div className="form-container">
-          <FormHeader/>
-          <SignupForm dispatch={dispatch}/>
-          <FormFooter text="Already have an account?" linkLabel="Login" linkTarget="/login"/>
-        </div>
-      )}
-    </StateContext.Consumer>
+    <div className="form-container">
+      <FormHeader/>
+      <SignupForm globalState={state} dispatch={dispatch}/>
+      <FormFooter text="Already have an account?" linkLabel="Login" linkTarget="/login"/>
+    </div>
   );
 }
